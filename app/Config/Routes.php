6822287306f2fc86cc458886d2378bbd34deb0c6
MickaElla <mickaellaandriamarondraibe@gmail.com>
2026-04-29 11:53:22 +0300

    liste 4

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -10,10 +10,8 @@ $routes->get('/', 'Home::index');
 $routes->get('/login',   'AuthController::login');
 $routes->post('/login',  'AuthController::doLogin');
 $routes->get('/dashboard', 'Home::dashboard');
-$routes->get('/list', 'Etudiant::index');
+$routes->get('/list', 'EtudiantController::index');
 $routes->get('/form', 'Home::form');
-$routes->get('/etudiants', 'Etudiant::index');
-$routes->get('/etudiant/notes/(:num)', 'Etudiant::notes/$1');
-$routes->get('/etudiants/(:num)/notes', 'Etudiant::notes/$1');
-
-
+$routes->get('/etudiants', 'EtudiantController::index');
+$routes->get('/etudiant/notes/(:num)', 'EtudiantController::notes/$1');
+$routes->get('/etudiants/(:num)/notes', 'EtudiantController::notes/$1');
